acbac1f12e72d0b6448db63561f035f9af70891a
David Galvez <davidgalvezgomez01@gmail.com>
2022-03-13 13:55:26 +0100

    Initialized server and DB

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
-const testTsWorksProperly: { devsInProject: number; isCoders: boolean } = {
-  devsInProject: 4,
-  isCoders: true,
-};
-testTsWorksProperly.devsInProject = 6;
+import dotenv from "dotenv";
+import initializeMongoDBServer from "./database";
+import { initializeServer } from "./server";
+
+dotenv.config();
+
+const port: number | string =
+  process.env.PORT ?? process.env.LOCAL_PORT ?? 5000;
+
+(async () => {
+  try {
+    await initializeServer(port);
+    await initializeMongoDBServer(process.env.MONGODB_STRING);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
---
+++ server/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import chalk from "chalk";
+import cors from "cors";
+import morgan from "morgan";
+import Debug from "debug";
+import IError from "../interfaces/error";
+import { generalErrorHandler, notFoundErrorHandler } from "./middlewares/error";
+
+const debug = Debug("jobs:server");
+
+export const app = express();
+
+export const initializeServer = (port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.yellow(`Listening at port number: ${port}`));
+      resolve(server);
+    });
+
+    server.on("error", (error: IError) => {
+      debug(chalk.red("Error at initialize server"));
+      if (error.code === "EADDRINUSE") {
+        debug(chalk.red(`Port number ${port} is already in use`));
+      }
+      reject();
+    });
+
+    server.on("close", () => {
+      debug(chalk.yellow("Express server disconnected"));
+    });
+  });
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(notFoundErrorHandler);
+app.use(generalErrorHandler);
