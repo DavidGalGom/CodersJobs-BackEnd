126202d9b868f91499154fef11e0425a58993a41
David Galvez <davidgalvezgomez01@gmail.com>
2022-03-13 12:37:48 +0100

    Added typeScript and jest to the project and their configs

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+  },
+  ignorePatterns: [".eslintrc.js", "./dist", "/*.config.js", "/*.config.cjs"],
+};
---
+++ index.ts
@@ -0,0 +1,5 @@
+const testTsWorksProperly: { devsInProject: number; isCoders: boolean } = {
+  devsInProject: 4,
+  isCoders: true,
+};
+testTsWorksProperly.devsInProject = 6;
