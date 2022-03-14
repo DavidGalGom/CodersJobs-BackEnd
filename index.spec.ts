790ffe91d98a7a427e160b27ed99af8ee1cc2d9b
David Galvez <davidgalvezgomez01@gmail.com>
2022-03-14 13:52:21 +0100

    Added tests for error middleware

--- index.spec.ts
+++
@@ -1,7 +0,0 @@
-describe("Given a project with gitHub workFlows", () => {
-  describe("When a branch is going to get merged", () => {
-    test("Then a successful test is needed", () => {
-      expect(true).toBe(true);
-    });
-  });
-});
