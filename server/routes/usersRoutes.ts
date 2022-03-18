f5a0c19b5c7d323e9a4206a6b184588ea1c6523e
David Galvez <davidgalvezgomez01@gmail.com>
2022-03-18 13:40:51 +0100

    Added getUsers function

--- server/routes/usersRoutes.ts
+++ server/routes/usersRoutes.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { loginUser, addUser } from "../controllers/usersControllers";
+import { loginUser, addUser, getUsers } from "../controllers/usersControllers";
+import adminAuth from "../middlewares/adminAuth";
+import auth from "../middlewares/auth";
 
 const router = express.Router();
 
 router.post("/register", addUser);
 router.post("/login", loginUser);
+router.get("/", auth, adminAuth, getUsers);
 
 export default router;
